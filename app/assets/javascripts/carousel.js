5444509b2d3a159cc2462418570bacefa7604649
GOCASE <gocase@Mac-mini-de-GOCASE.local>
2018-01-22 15:07:39 -0300

    carousel funcionando parcialmente e mudanças no layout da home

--- app/assets/javascripts/carousel.js
+++ app/assets/javascripts/carousel.js
@@ -1,22 +1,23 @@
-$(document).ready(function(){
-  $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    autoplay: true,
-    nav:false,
-    responsive:{
-        0:{
-            items:1,
-            autoplay: false
-        },
-        600:{
-            items:1,
-            autoplay: false
-        },
-        1000:{
-            items:1
+$(document).on('turbolinks:load', function () {
+    $('.owl-carousel').owlCarousel({
+        loop:false,
+        margin:10,
+        autoplay: true,
+        nav:false,
+        //autoWidth:true,
+        responsive:{
+            0:{
+                items:1,
+                autoplay: false
+            },
+            600:{
+                items:1,
+                autoplay: false
+            },
+            1000:{
+                items:1
+            }
         }
-    }
-  })
+      })
 });
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,34 +5,38 @@ class User < ApplicationRecord
   has_many :followers, :class_name => "Follow", :foreign_key => "user_id"
   has_many :following, :class_name => "Follow", :foreign_key => "follower_id"
 
-  validates :username, presence: :true, uniqueness: { case_sensitive: false }
+  attr_accessor :login
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, 
+         :validatable, authentication_keys: [:login]
+
 
   # Only allow letter, number, underscore and punctuation.
   validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
   validate :validate_username
 
-  attr_accessor :login
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
         
 
   def self.find_for_database_authentication(warden_conditions)
-  conditions = warden_conditions.dup
-  
-  if login = conditions.delete(:login)
-    where(conditions).where(["username = :value OR lower(email) = lower(:value)", { :value => login }]).first
-  elsif conditions.has_key?(:username) || conditions.has_key?(:email)
-    where(conditions.to_h).first
-  end
-  conditions[:email].downcase! if conditions[:email]
-  where(conditions.to_h).first
-
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(conditions).where(["username = :value OR lower(email) = lower(:value)", { :value => login }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
   end
 
-def validate_username
-  if User.where(email: username).exists?
-    errors.add(:username, :invalid)
+  validates :username, presence: :true, uniqueness: { case_sensitive: false }
+
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+
+  validate :validate_username
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
   end
-end
   
 end
