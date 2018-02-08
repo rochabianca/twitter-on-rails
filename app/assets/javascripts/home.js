f28ccc8191994821a1a9d8ce4c4806237b2be69d
Bianca <bianca.rocha1711@gmail.com>
2018-02-08 20:48:20 -0300

    timeline layout

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,3 +1,17 @@
 // # Place all the behaviors and hooks related to the matching controller here.
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
+
+$(document).on('click', '#menu__footer', function() {
+  $('#menu__footer').addClass('opened-menu');
+  $('#log-out-option').addClass('slideInUp');
+  $('#log-out-option').removeClass('essential__display-none');
+  
+  
+})
+
+$(document).on('click', '.opened-menu', function() {
+  $('#menu__footer').removeClass('opened-menu');
+  // $('#log-out-option').addClass('slideInDown').fadeIn('slow');
+  $('#log-out-option').addClass('essential__display-none').fadeIn('slow');
+})
--- app/assets/javascripts/tweets.coffee
+++ app/assets/javascripts/tweets.coffee
@@ -1,11 +1,5 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
 $(document).ready ->
   $("#timeline .timeline--page, #timeline--profile .timeline--page").infiniteScroll
     path: "nav.pagination a[rel=next]" # selector for the NEXT link (to page 2)
     append: ".timeline--page" # selector for all items you'll retrieve
 
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
