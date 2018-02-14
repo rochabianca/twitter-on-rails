af6fa4811612ad90bd05e36dea06c733f538683e
Bianca <bianca.rocha1711@gmail.com>
2018-02-13 21:39:34 -0300

    new tweet adapted for the new layout (no animations yet)

--- app/assets/javascripts/new-tweet.js
+++ app/assets/javascripts/new-tweet.js
@@ -2,7 +2,7 @@ var totalCaracteres = 140;
 
 $(document).on('turbolinks:load', function () {
   $('.caracteres').text(totalCaracteres);
-  $('.button-tweet').addClass('button--disabled');
+  $('.button__tweet').addClass('button__disabled');
 });
 
 $(document).on('keyup paste', '#tweet_content', function() {
@@ -14,17 +14,27 @@ $(document).on('keyup paste', '#tweet_content', function() {
   totalCaracteres = (140-valorTextarea);
 
   if (totalCaracteres < 16) {
-    $('.caracteres').addClass('text--danger');
+    $('.caracteres').addClass('text__danger');
   }
   if (totalCaracteres < 0 || totalCaracteres == 140) {
-    $('.button-tweet').addClass('button--disabled');
+    $('.button__tweet').addClass('button__disabled');
   }
   else {
     $('.caracteres').removeClass('text--danger');
-    $('.button-tweet').removeClass('button--disabled');
+    $('.button__tweet').removeClass('button__disabled');
   }
 
 
   $('.caracteres').text(totalCaracteres);
   }, 200)
+});
+
+$(document).on('click', '#open-tweet-model', function() {
+  $('.menu').addClass('menu__opened').fadeIn('slow');
+  $('.new-tweet').removeClass('essential__display-none').fadeIn('slow');
+});
+
+$(document).on('click', '#new-tweet__quit, #sendTweet', function() {
+  $('.menu').removeClass('menu__opened');
+  $('.new-tweet').addClass('essential__display-none');
 });
