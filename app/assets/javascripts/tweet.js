7a5a2f8817502a4dd6d4104073bf08760f8d8ecc
Bianca <bianca.rocha1711@gmail.com>
2018-02-22 22:35:33 -0300

    minimal changes on the layout of profile

--- app/assets/javascripts/tweet.js
+++ app/assets/javascripts/tweet.js
@@ -1,6 +1,8 @@
 $(document).on('mouseenter', '.tweet', function() {
-  $('.profile__delete-tweet', this).removeClass('essential__display-none').fadeIn('slow')
-  $('.tweet__actions', this).removeClass('essential__display-none').fadeIn('slow')
+  $('.profile__delete-tweet', this).removeClass('essential__display-none').fadeIn('slow');
+  $('.profile__delete-tweet', this).addClass('fadeInDown');
+  $('.tweet__actions', this).removeClass('essential__display-none').fadeIn('slow');
+  $('.tweet__actions', this).addClass('fadeInDown');
 });
 
 $(document).on('mouseleave', '.tweet', function() {
